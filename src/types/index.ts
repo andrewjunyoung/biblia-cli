c2bb49263156ba23c76b14c78c35ad5dfcfd276a
ajy <andrew@getinstant.app>
2025-10-04 20:03:38 -0400

    Type changes.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,20 @@
 export interface BibleVerse {
+  bibleId: string | undefined;
   book: string;
+  chapters: [string] | undefined;
+  verses: [string] | undefined;
   index: number;
   text: string;
 }
 
-export interface Text {
+export interface BibleVerseRequest {
+  bibleId: string | undefined;
+  book: string;
+  chapters: [string] | undefined;
+  verses: [string] | undefined;
+}
+
+export interface BibleText {
   original: string;
   transcription: string;
   translation: string;
